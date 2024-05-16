c00c65655d26e0c966946b6731da54a5cc5fc88d
Jack <jratermann@nevada.unr.edu>
2024-05-16 09:42:28 -0700

    These Are Great

--- text_editor/src/main.rs
+++ text_editor/src/main.rs
@@ -1,5 +1,8 @@
-use iced::widget::{text, container, text_editor};
-use iced::{Theme, Element, Sandbox, Settings};
+use iced::widget::{column, container, horizontal_space, row, text, text_editor};
+use iced::{executor, Application, Command, Element, Length, Settings, Theme};
+use std::io;
+use std::path::Path;
+use std::sync::Arc;
 
 pub fn main() -> iced::Result {
     Editor::run(Settings::default())
@@ -12,36 +15,64 @@ struct Editor {
 #[derive(Debug, Clone)]
 enum Message {
     Edit(text_editor::Action),
+    FileOpened(Result<Arc<String>, io::ErrorKind>),
 }
 
-impl Sandbox for Editor {
+impl Application for Editor {
     type Message = Message;
+    type Theme = Theme;
+    type Executor = executor::Default;
+    type Flags = ();
 
-    fn new() -> Self {
-        Self {
-            content: text_editor::Content::new(),
-        }
+    fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
+        (
+            Self {
+                content: text_editor::Content::new(),
+            },
+            Command::perform(
+                load_file(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR"),)),
+                Message::FileOpened,
+            ),
+        )
     }
 
     fn title(&self) -> String {
         String::from("Muse Editor 1.0")
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) -> Command<Message> {
         match message {
             Message::Edit(action) => {
                 self.content.perform(action);
             }
+            Message::FileOpened(result) => {
+                if let Ok(content) = result {
+                    self.content = text_editor::Content::with_text(&content);
+                }
+            }
         }
+
+        Command::none()
     }
 
     fn view(&self) -> Element<'_, Message> {
         let input = text_editor(&self.content).on_action(Message::Edit);
-        container(input).padding(15).into()
-
+        let position = {
+            let (line, column) = self.content.cursor_position();
+            text(format!("{}:{}", line + 1, column + 1))
+        };
+        let status_bar = row![horizontal_space().width(Length::Fill), position];
+        let ui = column![input, status_bar].spacing(10);
+        container(ui).padding(15).into()
     }
 
     fn theme(&self) -> Theme {
         Theme::Dracula
     }
+}
+async fn load_file(path: impl AsRef<Path>) -> Result<Arc<String>, io::ErrorKind> {
+    tokio::fs::read_to_string(path)
+        .await
+        .map(Arc::new)
+        .map_err(|error| error.kind())
 }
